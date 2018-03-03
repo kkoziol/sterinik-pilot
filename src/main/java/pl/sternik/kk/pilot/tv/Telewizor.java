4418e782fd05c9ce6c0bdb3a4fb7f1d3cb8b9c12
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-03 14:19:32 +0100

    Praca domowa TV do poprawki bo nie dziala

--- src/main/java/pl/sternik/kk/pilot/tv/Telewizor.java
+++ src/main/java/pl/sternik/kk/pilot/tv/Telewizor.java
@@ -2,39 +2,39 @@ package pl.sternik.kk.pilot.tv;
 
 public final class Telewizor {
     private static Telewizor instance = new Telewizor();
-
+    // nie działa do poprawki!
     public final TelewizorStan WYLACZONY = new TelewizorWylaczony(this);
     public final TelewizorStan WLACZONY = new TelewizorWlaczony(this);
 
-	private Telewizor() {
-		System.out.println("Tworzę sprzęt Telewizor");
-	};
+    private Telewizor() {
+        System.out.println("Tworzę sprzęt Telewizor");
+    };
 
-	public static Telewizor instance() {
-		return instance;
-	}
+    public static Telewizor instance() {
+        return instance;
+    }
 
-	public void wlacz() {
-		System.out.println("Włączam TV");
-	}
+    public void wlacz() {
+        System.out.println("Włączam TV");
+    }
 
-	public void wylacz() {
-		System.out.println("Wyłączam TV");
-	}
+    public void wylacz() {
+        System.out.println("Wyłączam TV");
+    }
 
-	public void kanalPlus() {
-		System.out.println("Następny kanał");
-	}
+    public void kanalPlus() {
+        System.out.println("Następny kanał");
+    }
 
-	public void kanalMinus() {
-		System.out.println("Poprzedni kanał");
-	}
+    public void kanalMinus() {
+        System.out.println("Poprzedni kanał");
+    }
 
-	public void glosnoscPlus() {
-		System.out.println("głosniej");
-	}
+    public void glosnoscPlus() {
+        System.out.println("głosniej");
+    }
 
-	public void glosnoscMinus() {
-		System.out.println("ciszej");
-	}
+    public void glosnoscMinus() {
+        System.out.println("ciszej");
+    }
 }
