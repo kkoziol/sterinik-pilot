b7f0f2db327218e5ca54643b8bf694c5ca0488f6
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-03 13:34:30 +0100

    Stany wentylatora

--- src/main/java/pl/sternik/kk/pilot/wentylator/Wentylator.java
+++ src/main/java/pl/sternik/kk/pilot/wentylator/Wentylator.java
@@ -3,6 +3,13 @@ package pl.sternik.kk.pilot.wentylator;
 public final class Wentylator {
     private static Wentylator instance = new Wentylator();
 
+    final WentylatorStan WYLACZONY = new WentylatorWylaczony(this);
+    final WentylatorStan OBROTY1 = new WentylatorObroty1(this);
+    final WentylatorStan OBROTY2 = new WentylatorObroty2(this);
+    final WentylatorStan OBROTY3 = new WentylatorObroty3(this);
+
+    private WentylatorStan aktualnyStan = WYLACZONY;
+
     private Wentylator() {
         System.out.println("Tworzę sprzęt Wentylator");
     };
@@ -12,22 +19,22 @@ public final class Wentylator {
     }
 
     public void wlacz() {
-        System.out.println("Włączam Wentylator");
+        aktualnyStan = aktualnyStan.wlacz();
     }
 
     public void wylacz() {
-        System.out.println("wyłączam wentylator");
+        aktualnyStan = aktualnyStan.wylacz();
     }
 
     public void obroty1() {
-        System.out.println("Obroty na 1");
+        aktualnyStan = aktualnyStan.obroty1();
     }
 
     public void obroty2() {
-        System.out.println("obroty na 2");
+        aktualnyStan = aktualnyStan.obroty2();
     }
 
     public void obroty3() {
-        System.out.println("obroty na 3");
+        aktualnyStan = aktualnyStan.obroty3();
     }
 }
